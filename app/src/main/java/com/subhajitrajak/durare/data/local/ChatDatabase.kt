7af91560d372ea7a2bdabb16ac7e9758f6d5f5b6
Subhajit Rajak <subhajitrajak2001@gmail.com>
2025-11-09 01:20:53 +0530

    Chore: Disable schema export in Room database
    This commit updates the `ChatDatabase` configuration to disable the exporting of the database schema.
    
    Key changes:
    - **`app/src/main/java/com/subhajitrajak/durare/data/local/ChatDatabase.kt`**:
      - Added `exportSchema = false` to the `@Database` annotation to prevent Room from exporting the schema, as it is not needed for this project.

--- app/src/main/java/com/subhajitrajak/durare/data/local/ChatDatabase.kt
+++ app/src/main/java/com/subhajitrajak/durare/data/local/ChatDatabase.kt
@@ -3,7 +3,7 @@ package com.subhajitrajak.durare.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ChatMessageEntity::class], version = 1)
+@Database(entities = [ChatMessageEntity::class], version = 1, exportSchema = false)
 abstract class ChatDatabase : RoomDatabase() {
     abstract fun chatDao(): ChatDao
 }
