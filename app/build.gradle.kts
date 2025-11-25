a89f253d61572f670b675bb69a9c99c70de4203d
Subhajit Rajak <subhajitrajak2001@gmail.com>
2025-11-25 14:32:04 +0530

    Chore: Bump version to 0.5.0
    This commit prepares for a new release by incrementing the application version.
    
    Key changes:
    - **`app/build.gradle.kts`**:
      - The `versionCode` has been updated from `16` to `17`.
      - The `versionName` has been updated from `0.4.1` to `0.5.0`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.subhajitrajak.durare"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.4.1"
+        versionCode = 17
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
