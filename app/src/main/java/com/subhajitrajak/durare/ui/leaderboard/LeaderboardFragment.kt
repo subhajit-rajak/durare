e7247be568f24468f9b970da4d5260a67a6440e4
Subhajit Rajak <subhajitrajak2001@gmail.com>
2025-12-06 14:26:36 +0530

    Fix: Load leaderboard data when the view is created
    This commit ensures that the leaderboard data is fetched and displayed when the `LeaderboardFragment` is created by calling `viewModel.loadLeaderboard()` in the `onViewCreated` method.

--- app/src/main/java/com/subhajitrajak/durare/ui/leaderboard/LeaderboardFragment.kt
+++ app/src/main/java/com/subhajitrajak/durare/ui/leaderboard/LeaderboardFragment.kt
@@ -42,6 +42,8 @@ class LeaderboardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.loadLeaderboard()
+
         adapter = LeaderboardAdapter(requireContext())
         binding.leaderboard.adapter = adapter
 
